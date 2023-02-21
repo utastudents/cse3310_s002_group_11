a71334d8531342a11f97c70f60e5fbb4d425484e
Christopher Worley <123421172+cgw8000@users.noreply.github.com>
2023-02-21 12:19:55 +0000

    Sanity Checks Adding

--- src/extfat.c
+++ src/extfat.c
@@ -12,15 +12,37 @@
 #include <sys/stat.h>
 
 // Macro Declarations
-#define isZero(x) (x == 0)
-#define isNull(x) (x == NULL)
-#define isTrue(x) (x == true)
-#define isFalse(x) (x == false)
-#define isFault(x) (x == -1)
-#define setFunction(x) x->function=__func__
+#define isZero(x) (x == 0) // Boolean test for being 0
+#define isNull(x) (x == NULL) // Boolean test for being NULL
+#define isTrue(x) (x == true) // Boolean test for being true
+#define isFalse(x) (x == false) // Boolean test for being false
+#define isFault(x) (x == -1) // Boolean test for being -1
+#define isNEQ(x,y) (x != y) // Boolean test for being unequal
+#define isEQ(x,y) (x != y) // Boolean test for being equal
+#define setFunction(x) x->function=__func__ // Set instance structure function to current function
+// Inline version of:
+// void compareMemory(bool u, struct instance * v, struct instance * w, [struct insance member, doesn't translate] x, int y, char * z)
+// {
+//    if (memcmp (&(v->x), &(w->x), y) != 0)
+//    {
+//        OK = false;
+//        printf ("%s: Field %s is inconsistant and too messy to verbose print\n", inst->function, z);
+//        OK = u;
+//    }
+// }
 #define compareMemory(u, v, w, x, y, z) { if (memcmp (&(v->x), &(w->x), y) != 0) {OK = false; printf ("%s: Field %s is inconsistant and too messy to verbose print\n", inst->function, z); OK = u; }}
+// Inline version of:
+// void compareValues(bool v, struct instance * w, struct instance * x, [struct insance member, doesn't translate] y, char * z)
+// {
+//     if (w->y != x->y)
+//     {
+//         OK = false;
+//         printf ("%s: Field %s is inconsistant [Main:%ld != %ld:Backup]\n", inst->function, z, (__uint64_t)(w->y), (__uint64_t)(x->y));
+//         OK = v;
+//     }
+// }
 #define compareValues(v, w, x, y, z) { if (w->y != x->y) {OK = false; printf ("%s: Field %s is inconsistant [Main:%ld != %ld:Backup]\n", inst->function, z, (__uint64_t)(w->y), (__uint64_t)(x->y)); OK = v; }}
-#define limitCheck(x,y,z) ((x >= y) && (x <= z))
+#define limitCheck(x,y,z) ((x >= y) && (x <= z)) // Boolean test for x >= y >= z
 
 // Map the files from the main memory structure
 int mapFile (struct instance * inst)
@@ -56,6 +78,45 @@ int mapFile (struct instance * inst)
         printf ("%s: Cannot map output file to memory\n", inst->function);
         return EXIT_FAILURE;
     }
+    if (!limitCheck(inst->bootSectorMain->bytesPerSectorShift, 5, 9))
+    {
+        fprintf (stderr, "%s: Illegal value of %d for BytesPerSectorShift\n", inst->function, inst->bootSectorMain->bytesPerSectorShift);
+        return EXIT_FAILURE; 
+    }
+    if (isNEQ(inst->bootSectorMain->jumpBoot[0], 0xEB) && \
+        isNEQ(inst->bootSectorMain->jumpBoot[1], 0x76) && \
+        isNEQ(inst->bootSectorMain->jumpBoot[2], 0x90))
+    {
+        fprintf (stderr, "%s: JumpBoot value [%02x][%02x][%02x] is invalid\n", inst->function, inst->bootSectorMain->jumpBoot[0],\
+        inst->bootSectorMain->jumpBoot[1], inst->bootSectorMain->jumpBoot[2]);
+        return EXIT_FAILURE;
+    }
+    for (int i = 0; i < 390; i++)
+    {
+        if (isNEQ(inst->bootSectorMain->mustBeZero[i], 0))
+        {
+            fprintf (stderr, "%s: MustBeZero value is not zero at offset %d", inst->function, i);
+            return EXIT_FAILURE;
+        }
+    }
+    if (inst->bootSectorMain->volumeLength < (1 << (20 - inst->bootSectorMain->bytesPerSectorShift)))
+    {
+        fprintf (stderr, "%s: VolumeLength field is too small [%ld]", inst->function, inst->bootSectorMain->volumeLength);
+        return EXIT_FAILURE;
+    }
+    if (!limitCheck(inst->bootSectorMain->fatOffset, 24, \
+        inst->bootSectorMain->clusterHeapOffset - (inst->bootSectorMain->fatLength * inst->bootSectorMain->numberOfFats)))
+    {
+        fprintf (stderr, "%s: FatOffset field is invalid [%d]", inst->function, inst->bootSectorMain->fatOffset);
+        return EXIT_FAILURE;
+    }
+    if (!limitCheck(inst->bootSectorMain->fatLength, \
+        (inst->bootSectorMain->clusterCount + 2) * (1 << (inst->bootSectorMain->bytesPerSectorShift - 2)), \
+        (inst->bootSectorMain->clusterHeapOffset - inst->bootSectorMain->fatOffset) / inst->bootSectorMain->numberOfFats))
+    {
+        fprintf (stderr, "%s: FatLength field is invalid [%d]", inst->function, inst->bootSectorMain->fatLength);
+        return EXIT_FAILURE;
+    }
     return EXIT_SUCCESS;
 }
 
@@ -63,10 +124,10 @@ int mapFile (struct instance * inst)
 int unmapFile (struct instance * inst)
 {
     setFunction (inst);
-    msync (inst->memInput, inst->inFile.st_size, MS_SYNC);
+    msync (inst->memInput, inst->inFile.st_size, MS_SYNC); // Merged from Phu
     munmap (inst->memInput, inst->inFile.st_size);
     close (inst->fdInput);
-    msync (inst->memOutput, inst->outFile.st_size, MS_SYNC);
+    msync (inst->memOutput, inst->outFile.st_size, MS_SYNC); // Merged from Phu
     munmap (inst->memInput, inst->inFile.st_size);
     close (inst->fdOutput);
     return EXIT_SUCCESS;
@@ -111,6 +172,8 @@ void closeExfat (struct instance * inst)
 }
 
 // Initial Boot Sector Read (First 512 bytes)
+// Need to transition to fread
+// Need to fix various small errors
 int readBootSector (struct instance * inst)
 {
     int val = 0;
@@ -202,9 +265,9 @@ int main(int argc, char ** argv)
         "           -i xxx    where xxx is the input file name [This is optional, but -i test.image is implied if not specified]",
         "           -o xxx    where xxx is the output file number [This is optional, inputFile will be used if not specified]",
         "           -c        triggers the copying of input to output (This is optional)",
-        "           -m        use mmap for file access. [implied if -f and -m not specified]", // Added from rency
-        "           -f        use fread for file access", // Addded from rency
-        "           -v        verify exfat image", // Added from rency
+        "           -m        use mmap for file access. [implied if -f and -m not specified]", // Added from Rency
+        "           -f        use fread for file access", // Addded from Rency
+        "           -v        verify exfat image", // Added from Rency
         "           -h        is this help message",
         NULL
     };    
@@ -244,13 +307,13 @@ int main(int argc, char ** argv)
                 if (optopt == 'i' || optopt == 'o')
                 fprintf (stderr, "Option requires an argument.\n");
                 return EXIT_FAILURE;
-            case 'm': // Added from rency
+            case 'm': // Added from Rency
                 exfat.mflag = true;
                 break;
-            case 'f': // Added from rency
+            case 'f': // Added from Rency
                 exfat.fflag = true;
                 break;
-            case 'v': // Added from rency
+            case 'v': // Added from Rency
                 exfat.vflag = true;
                 break;
             case 'h':
@@ -269,6 +332,11 @@ int main(int argc, char ** argv)
     if (isFalse(exfat.iflag)) exfat.ivalue = "test.image";
     if (isFalse(exfat.oflag)) exfat.ovalue = exfat.ivalue;
     if (isFalse(exfat.fflag) && isFalse(exfat.mflag)) exfat.mflag = true;
+    if (isTrue(exfat.fflag) && isTrue(exfat.mflag)) // Added from Rency
+    {
+        fprintf (stderr, "-f and -m options are mutually exclusive");
+        return EXIT_FAILURE;
+    }
     if (mapFile (&exfat) == EXIT_FAILURE)
     {
         unmapFile (&exfat);
