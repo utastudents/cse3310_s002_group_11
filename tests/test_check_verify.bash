20c10ac37b8d6ca4b8e64dcc827bdeef6ec7a484
pht23 <98304090+pht23@users.noreply.github.com>
2023-04-07 03:29:24 +0000

    Task 2

--- tests/test_check_verify.bash
+++ tests/test_check_verify.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Test to check verify input file"
 
-./extfat -v -i test.image
+./extfat -v 
 
-if [ $? -eq 0 ]; then
-    echo "Successfully verified input file"
+if [ $? -eq 1 ]; then
+    echo "Successfully checked for an input file"
     result=0
 else
     result=1
